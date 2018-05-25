a2888c6eb242099a2558d012f0a96ae144cfd51b
Garrett Van Beek <garrettvanbeek@gmail.com>
2018-05-25 15:48:16 -0600

    Created a delete tables sql for testing purposes, copy paste to use.

---
+++ db/delete tables.sql
@@ -0,0 +1,7 @@
+DROP TABLE "HypoxicCR"."public"."Pagos (join Table)";
+DROP TABLE "HypoxicCR"."public"."Info_atletas (bridge table)";
+DROP TABLE "HypoxicCR"."public"."Atletas";
+DROP TABLE "HypoxicCR"."public"."Entrenadores";
+DROP TABLE "HypoxicCR"."public"."Fechas_de_entrega";
+DROP TABLE "HypoxicCR"."public"."Paquetes";
+DROP TABLE "HypoxicCR"."public"."Patrocinadores";
