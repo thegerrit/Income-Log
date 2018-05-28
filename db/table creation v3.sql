2463b7b36dc73cabd2518a1c7e62d0c2cfacb643
Garrett Van Beek <garrettvanbeek@gmail.com>
2018-05-28 16:48:49 -0600

    Added SQL to populate data tables. Almost done with it.

--- db/table creation v3.sql
+++ db/table creation v3.sql
@@ -6,7 +6,7 @@ CREATE TABLE "HypoxicCR"."public"."DEPORTE" (
 -- don't allow DEPORTE to be deleted for relational integrity (ri)
 
 CREATE TABLE "HypoxicCR"."public"."ENTRENADOR" (
-  "id" INTEGER NOT NULL,
+  "id" SERIAL,
   "nombre" VARCHAR(50) NOT NULL,
   "apellidos" VARCHAR(50) NOT NULL,
   "email" VARCHAR(100) NOT NULL,
@@ -29,8 +29,8 @@ CREATE TABLE "HypoxicCR"."public"."PAQUETE" (
   "id_deporte" INTEGER NOT NULL,
   "id_entrenador" INTEGER NOT NULL,
   PRIMARY KEY ("id"),
-  FOREIGN KEY ("id_deporte") REFERENCES "DEPORTE"("id"),
-  FOREIGN KEY ("id_entrenador") REFERENCES "ENTRENADOR"("id")
+  FOREIGN KEY ("id_deporte") REFERENCES "DEPORTE"("id") ON DELETE RESTRICT,
+  FOREIGN KEY ("id_entrenador") REFERENCES "ENTRENADOR"("id") ON DELETE RESTRICT
 );
 
 CREATE TABLE "HypoxicCR"."public"."ATLETA" (
@@ -41,8 +41,8 @@ CREATE TABLE "HypoxicCR"."public"."ATLETA" (
   "id_paquete" INTEGER NOT NULL,
   "id_patrocinador" INTEGER,
   PRIMARY KEY ("cedula"),
-  FOREIGN KEY ("id_paquete") REFERENCES "PAQUETE"("id"),
-  FOREIGN KEY ("id_patrocinador") REFERENCES "PATROCINADOR"("id")
+  FOREIGN KEY ("id_paquete") REFERENCES "PAQUETE"("id") ON DELETE RESTRICT,
+  FOREIGN KEY ("id_patrocinador") REFERENCES "PATROCINADOR"("id") ON DELETE RESTRICT
 );
 
 CREATE TABLE "HypoxicCR"."public"."PAGO" (
@@ -56,7 +56,7 @@ CREATE TABLE "HypoxicCR"."public"."PAGO" (
   "monto" INTEGER NOT NULL,
   "diferencia" INTEGER NOT NULL,
   PRIMARY KEY ("id"),
-  FOREIGN KEY ("cedula_atleta") REFERENCES "ATLETA"("cedula"),
-  FOREIGN KEY ("id_paquete") REFERENCES "PAQUETE"("id"),
-  FOREIGN KEY ("id_patrocinador") REFERENCES "PATROCINADOR"("id")
+  FOREIGN KEY ("cedula_atleta") REFERENCES "ATLETA"("cedula") ON DELETE RESTRICT,
+  FOREIGN KEY ("id_paquete") REFERENCES "PAQUETE"("id") ON DELETE RESTRICT,
+  FOREIGN KEY ("id_patrocinador") REFERENCES "PATROCINADOR"("id") ON DELETE RESTRICT
 );
