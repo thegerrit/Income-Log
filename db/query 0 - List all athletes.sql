831e439606d3bb4431b71b2523d014430916a732
Garrett Van Beek <garrettvanbeek@gmail.com>
2018-05-29 14:30:25 -0600

    Updated 'List all atheletes.sql' to standards specified on Trello.

--- db/query 0 - List all athletes.sql
+++ db/query 0 - List all athletes.sql
@@ -1,4 +1,4 @@
-SELECT a."cedula",a."nombre",a."apellidos", a."email", a."id_paquete", paq."nombre", paq."precio", d."nombre" AS "deporte", e."nombre" AS "entrenador_nombre", e."apellidos" AS "entrenador_apellidos"
+SELECT a."apellidos",a."nombre",a."cedula", a."email",pat."apellidos" AS "patrocinador_apellidos",pat."nombre" AS "patrocinador_nombre",pat."nombre_organizacion" AS "patrocinador_organizacion",pat."email" AS "patrocinador_email",d."nombre" AS "deporte", paq."nombre",e."apellidos" AS "entrenador_apellidos",e."nombre" AS "entrenador_nombre"
 FROM "HypoxicCR"."public"."ATLETA" a
 LEFT JOIN "HypoxicCR"."public"."PAQUETE" paq
 ON a."id_paquete"=paq."id"
@@ -6,4 +6,6 @@ LEFT JOIN "HypoxicCR"."public"."DEPORTE" d
 ON paq."id_deporte"=d."id"
 LEFT JOIN "HypoxicCR"."public"."ENTRENADOR" e
 ON paq."id_entrenador"=e."id"
+LEFT JOIN "HypoxicCR"."public"."PATROCINADOR" pat
+ON a."id_patrocinador"=pat."id"
 ORDER BY a."apellidos";
