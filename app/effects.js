689e3ced6c9657e257c94830a5ce7eec5f5a44fc
Garrett Van Beek <garrettvanbeek@gmail.com>
2018-06-08 10:59:22 -0600

    checkAll is now fully functional. Select menus styled to the page.

--- app/effects.js
+++ app/effects.js
@@ -5,6 +5,13 @@ function hideDrawer () {
  function checkAll (source) {
   var checkboxes = document.getElementsByName('c_mes');
   for (var i = 0, n = checkboxes.length; i < n; i++) {
-    checkboxes[i].checked = source.checked
+    checkboxes[i].checked = source.checked;
+    var labelMes = document.getElementsByName('l_mes');
+    if (checkboxes[i].checked == true) {
+      labelMes[i].classList.add("is-checked");
+    }
+    else {
+      labelMes[i].classList.remove("is-checked");
+    }
   }
 }
