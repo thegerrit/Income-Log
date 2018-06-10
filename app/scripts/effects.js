611158c19ac9d575c69ed84dee07edbaa3c00cce
David Quiros <david@quiros.online>
2018-06-10 12:16:33 -0600

    Unify headers into one main app header

--- app/scripts/effects.js
+++ app/scripts/effects.js
@@ -1,11 +1,11 @@
 function hideDrawer () {
-  var drawer = document.getElementById('left-drawer');
+  var drawer = document.getElementById('details');
   var showButton = document.getElementById('show-button');
   drawer.classList.add('hidden');
   showButton.classList.remove('hidden');
 };
 function showDrawer(){
-  var drawer = document.getElementById('left-drawer');
+  var drawer = document.getElementById('details');
   var showButton = document.getElementById('show-button');
   drawer.classList.remove('hidden');
   showButton.classList.add('hidden');
