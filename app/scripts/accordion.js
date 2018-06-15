ba5171ae8b5eaef61952d270ee0dcc79ba25461c
Garrett Van Beek <garrettvanbeek@gmail.com>
2018-06-15 09:22:32 -0600

    Animated accordion.

--- app/scripts/accordion.js
+++ app/scripts/accordion.js
@@ -1,29 +1,9 @@
-/*var acc = document.getElementsByClassName("accordion-header");
-var i;
-
-for (i=0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    var panel = document.getElementById("accordion-content");
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    }
-  });
-}*/
 function accordion() {
-  /*var panel = event.target.parentElement.parentElement.nextElementSibling;
-  panel.classList.toggle("hidden");
-  if (panel.style.maxHeight){
-    panel.style.maxHeight = null;
-  } else {
-    panel.style.maxHeight = 600;
-  }*/
-  var panel= document.getElementById('accordion');
-  if (panel.style.maxHeight !== 0){
+  var panel= event.target.parentElement.parentElement.nextElementSibling;
+  if (panel.style.maxHeight !== "0px"){
     panel.style.maxHeight = 0;
   }
-  else if(panel.style.maxHeight === 0){
+  else {
     panel.style.maxHeight = panel.scrollHeight + "px";
   }
 };
