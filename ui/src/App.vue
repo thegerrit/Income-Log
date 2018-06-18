30e123dbd8eac5b4ab62852232abcf872b41fa32
David Quiros <david@quiros.online>
2018-06-18 12:34:53 -0600

    Create vue project to develop UI

---
+++ ui/src/App.vue
@@ -0,0 +1,91 @@
+<template>
+  <div id="app">
+    <AppHeader />
+    <img src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import AppHeader from "./components/AppHeader.vue";
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "app",
+  components: {
+    AppHeader,
+    HelloWorld
+  }
+};
+</script>
+
+<style lang="scss">
+:root {
+  --header-height: 48px;
+  --aside-width: 32vw;
+
+  --color-section-title: #207d7d;
+  --color-table-header: white;
+  --border-table-header: #135c6d;
+  --text-color-table-header: #614545;
+  --color-table-even-row: #f2f2f2;
+  --color-table-hover: #dcdede;
+
+  --color-cancel-button: #de6666;
+  --color-cancel-button-hover: #e61111;
+
+  --tg-border: #76efef;
+}
+
+body {
+  color: #5b5b5b;
+  font-family: "Roboto", sans-serif;
+}
+
+button.mdl-button--icon {
+  height: 48px;
+  width: 48px;
+}
+
+.mdl-button--primary {
+  background-color: var(--color-section-title);
+}
+
+.hidden {
+  /*display: none !important;*/
+  max-height: 0px;
+}
+
+/* width */
+::-webkit-scrollbar {
+  width: 10px;
+  height: 10px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #f1f1f1;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #135c6d;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #0cd2c9;
+}
+
+/* Datepicker */
+::-webkit-datetime-edit,
+::-webkit-datetime-edit-fields-wrapper,
+::-webkit-datetime-edit-text,
+::-webkit-datetime-edit-month-field,
+::-webkit-datetime-edit-day-field,
+::-webkit-datetime-edit-year-field,
+::-webkit-inner-spin-button,
+::-webkit-calendar-picker-indicator {
+  font-family: Roboto, sans-serif;
+}
+</style>
---
+++ ui/src/components/AppHeader.vue
@@ -0,0 +1,44 @@
+<template>
+  <header>
+    <nav>
+      <button class="mdl-button--icon mdl-button mdl-js-button mdl-js-ripple-effect hidden show-drawer" title="Mostrar detalles"
+        onclick="showDrawer()">
+        <i class="material-icons">menu</i>
+      </button>
+      <button class="mdl-button--icon mdl-button mdl-js-button mdl-js-ripple-effect hide-drawer" title="Ocultar detalles"
+        onclick="hideDrawer()">
+        <i class="material-icons">arrow_back_ios</i>
+      </button>
+    </nav>
+    <h1>Income Log</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+header {
+  display: flex;
+  background-color: rgb(33, 150, 243);
+  color: white;
+  box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 3px 1px -2px rgba(0, 0, 0, 0.2),
+    0 1px 5px 0 rgba(0, 0, 0, 0.12);
+  padding: 0 20px;
+}
+
+header h1 {
+  font-size: 1.8rem;
+  margin: 0;
+  flex: 1;
+  text-align: right;
+  line-height: var(--header-height);
+}
+</style>
