f524eaa0344648f40e9cb356e466c7ca0b1ac501
Garrett Van Beek <garrettvanbeek@gmail.com>
2018-06-20 17:42:55 -0600

    Styled results table additionally.

--- ui/src/components/ResultsPanel.vue
+++ ui/src/components/ResultsPanel.vue
@@ -133,7 +133,7 @@
       </div>
     </form>
 
-    <div id="spreadsheet">
+    <div class="table-container">
       <table class="mdl-data-table mdl-js-data-table mdl-shadow--2dp">
         <tr>
           <th>Apellidos</th>
@@ -237,5 +237,38 @@ export default {
       grid-area: report-actions;
       margin: 10px 0 20px;
     }
+  }
+  /* TODO: add max-height to container once ui is complete*/
+  .table-container{
+    width: 96%;
+    overflow: auto;
+    margin-left: 2%;
+    table{
+      width: 100%;
+    }
+  }
+  /*Shared Styles*/
+  .mdl-data-table {
+    th{
+      background-color: var(--color-table-header);
+      color: var(--text-color-table-header);
+      border-bottom: 2px solid var(--border-table-header);
+    }
+    tr{
+      &:nth-child(even){
+        background-color: var(--color-table-even-row);
+      }
+      &:hover{
+        background-color: var(--color-table-hover);
+      }
+    }
+
+  }
+  .table-container{ /*search-table and spreadsheet in overview.css*/
+    border: 5px solid #207d7d;
+    border-radius: 4px;
+    box-shadow: 2px 2px 4px #2196f3;
+    overflow-x: auto;
+    padding: 0;
   }
 </style>
