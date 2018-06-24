cfa0b9ad8f97e6846bcdc5d2027d0a0b0ed25568
David Quiros <david@quiros.online>
2018-06-24 14:16:10 -0600

    Remove scroll bar styles specific to webkit browsers
    Comment out HelloWorld component

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -2,8 +2,8 @@
   <div id="app">
     <AppHeader />
     <ContentArea></ContentArea>
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
@@ -11,14 +11,11 @@
 import AppHeader from "./components/AppHeader.vue";
 import ContentArea from "./components/ContentArea.vue";
 
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "app",
   components: {
     AppHeader,
-    ContentArea,
-    HelloWorld
+    ContentArea
   }
 };
 </script>
@@ -82,38 +79,5 @@ button.mdl-button--icon {
 
 .hidden {
   max-height: 0px;
-}
-
-/* width */
-::-webkit-scrollbar {
-  width: 10px;
-  height: 10px;
-}
-
-/* Track */
-::-webkit-scrollbar-track {
-  background: #f1f1f1;
-}
-
-/* Handle */
-::-webkit-scrollbar-thumb {
-  background: #135c6d;
-}
-
-/* Handle on hover */
-::-webkit-scrollbar-thumb:hover {
-  background: #0cd2c9;
-}
-
-/* Datepicker */
-::-webkit-datetime-edit,
-::-webkit-datetime-edit-fields-wrapper,
-::-webkit-datetime-edit-text,
-::-webkit-datetime-edit-month-field,
-::-webkit-datetime-edit-day-field,
-::-webkit-datetime-edit-year-field,
-::-webkit-inner-spin-button,
-::-webkit-calendar-picker-indicator {
-  font-family: Roboto, sans-serif;
 }
 </style>
